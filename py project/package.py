f3a2fcee8b912c38b774b3c0a15ff0d54b874f44
oxyblade <73794674+oxyblade-1@users.noreply.github.com>
2021-12-07 23:29:13 +0100

    Update package.py

--- py project/package.py
+++ py project/package.py
@@ -1,7 +1,15 @@
-import requests
+#https://mail.google.com/mail/u/0/#inbox
+#dire le titre de cette page
+#afficher la classe "gb_Ca gbii"
 
+import requests
 from bs4 import BeautifulSoup
 
-url = "https://github.com/oxyblade-1"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+url = "https://www.gov.uk/search/news-and-communications"
+link = requests.get(url)
+page = link.content
+soup = BeautifulSoup(page, "html.parser")
+descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
+
+print(soup.title.string)
+print(descriptions)
